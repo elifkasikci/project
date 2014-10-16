60d0b8327ed43de5f2cb975395bbb822cdd5a11c
ilknurbalci <ilknurbalc@gmail.com>
2014-10-16 15:27:07 +0300

    new login page

--- module/User/src/User/Model/User.php
+++ module/User/src/User/Model/User.php
@@ -15,46 +15,26 @@ class User {
 
      public function __construct(){
         $this->_connection = pg_connect("host=localhost dbname=postgres port=5432 user=postgres password=1234");
-//        $result=pg_query($connection,"select* from members ");
-//
-//        $members=array();
-//
-//        session_start();
-//        $id=$list["id"];
-//
-//        $email=$list["email"];
-//        $_SESSION['email']=$email;
-//
-//        $password=$list["password"];
-//        $_SESSION['password']=$password;
-//
-//        $birthday=$list['birthday'];
-//
-//
-//        $members[]=array("id"=>$id ,"email"=>$email,"password"=>$password,'birthday'=>$birthday);
-//
-//
-//        return $result;
+
     }
 
 
     public function checkUser($data) {
-       $user= $_SESSION['user'] ;
-
-        $result=pg_query($this->_connection,"select * from members where email='$user' ");
-        $result = pg_fetch_all($result);
-        return $result;
+       $user= $_SESSION['email'] ;
+       $password_form=$_SESSION['password'];
 
 
-    }
-
-
-    public function listAllUsers(){
-        $user= $_SESSION['user'] ;
-        $result=pg_query($this->_connection,"select * from members where id=1 ");
-        $result = pg_fetch_all($result);
-        return $result;
+       $result=pg_query($this->_connection,"select * from members where email='$user' and password='$password_form'  ");
+       $result = pg_fetch_all($result);
+//
+//       if(($email==$user) and ($password==$password)){
+//
+//        }
+       return $result;
 
     }
+
+
+
 
 }
