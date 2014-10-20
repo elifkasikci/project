e9ed9296224d1a9c6e442173d04cdd5ddb2bca37
Elif Kasikci <elifkasikci@users.noreply.github.com>
2014-10-20 09:37:57 +0300

    Revert "checkuser"

--- module/User/src/User/Controller/IndexController.php
+++ module/User/src/User/Controller/IndexController.php
@@ -13,56 +13,44 @@ namespace User\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 use User\Model\User as User;
-use Zend\Mvc\Controller\Plugin\Forward;
+
 
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        return new ViewModel();
 
-    }
-
-    public function loginAction()
-    {
         $viewData = Array();
 
+        if($_SERVER['REQUEST_METHOD'] == "GET"){
+//            $userModel = new User();
+//            $result = $userModel->listAllUsers();
+//            $viewData['type'] = "list";
+//            $viewData['data'] = $result;
+//            return new ViewModel(array('view' => $viewData));
+        }else{
         $information = Array();
-        $information['email'] = $_POST['email_form'];
-        $information['password'] = $_POST['password_form'];
+        $information['user'] = $_POST['email_form'];
+        $_SESSION['user']=$information['user'];
+        $information['password']= $_POST['password_form'];
 
-        $userModel = new User();
-        $result = $userModel->checkUser($information);
-
-
-        if ($result == true) {
-
-            $viewData['login'] = true;
+            $userModel = new User();
+            $result = $userModel->checkUser($information);
+            $viewData['type'] = "list";
             $viewData['data'] = $result;
-            session_start();
-            $_SESSION['id'] = $result[0]['id'];
             return new ViewModel(array('view' => $viewData));
 
-        } elseif($result == false) {
 
 
-
-          return  $this-> forward('page-not-found', 'error');
         }
+   //if condition
 
 
-    }
-
-    public function showAction()
-    {
-        session_start();
-        $userModel = new User();
-        $result = $userModel->getUser($_SESSION['id']);
-        return new ViewModel(array('user' => $result[0]));
 
     }
 
-   
+
+
 
 
 }
